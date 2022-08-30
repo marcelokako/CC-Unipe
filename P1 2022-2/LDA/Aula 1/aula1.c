c9af8753779378e039447d71a3a3ead7713cf256
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-08-29 21:04:39 -0300

    inclui assunto aula2 de TDA
    inclui potenciação e biblioteca math.h

--- P1 2022-2/LDA/Aula 1/aula1.c
+++ P1 2022-2/LDA/Aula 1/aula1.c
@@ -1,5 +1,6 @@
 #include <stdio.h>  //standard input and output
 #include <stdlib.h>  //standard library
+#include <math.h> //grande parte das operações matemáticas
 
 int main(void)  //assinatura do método principal 
 { //inicio do escopo
@@ -8,12 +9,15 @@ int main(void)  //assinatura do método principal
 
 	//Variável é uma representação de um endereço de memória
 	float	a; //declaração de variável
-	a = 2.3; //atribuição de variável
+	a = 2; //atribuição de variável
 	
-	float b = 3.1; //declarar + atribuir = inicialização
+	float b = 3; //declarar + atribuir = inicialização
 	
 	float soma = a + b;
-	
-	printf("Resultado da soma: %f + %f = %f", a, b, soma);
-	
+	float pot = pow(a,b); //a^b
+		
+	printf("Resultado da soma: %.2f + %.2f = %.2f \n", a, b, soma);
+	printf("Resultado da potencia: %.2f ^ %.2f = %f", a, b, pot);
+
+return 0;
 } //fim do escopo
