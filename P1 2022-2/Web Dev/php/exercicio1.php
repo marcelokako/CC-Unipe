4afd032039a861596f49959a3eeaefaf958587e6
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-09-14 20:38:10 -0300

    Update exercicio1.php
    adiciona teoria adicional obtida na segunda aula sobre php

--- P1 2022-2/Web Dev/php/exercicio1.php
+++ P1 2022-2/Web Dev/php/exercicio1.php
@@ -1,9 +1,20 @@
 <?php
 
-$nome = "João Pedro";
-$nota1 = 8.0;
-$nota2 = 10;
+    define("mat", 1234567890);
+    $nome = "Marcelo";
+    $nota1 = 10;
+    $nota2 = 9.0;
+    // concatenação --> .
+    echo "Nome: $nome, Matricula: " . mat . " <br> Nota 1:$nota1 <br> Nota 2: $nota2";
+    
+    // operadores aritmeticos
+    $media = ($nota1 + $nota2)/2;
+    echo "<br> Média: $media <br>";
 
-echo "Aluno $nome, sua primeira nota foi $nota1 e sua segunda nota foi $nota2";
+    // operadores relacionais: além dos usuais temos === --> idêntico
+    echo "<hr>";
+    var_dump('3' == 3);
+    var_dump('3' === 3);  
+    echo "<hr>";
 
 ?>
