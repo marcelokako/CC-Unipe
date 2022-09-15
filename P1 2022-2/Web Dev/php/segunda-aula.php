05525dc9567fdd980e9a5578530e60b22291a34a
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-09-14 21:41:33 -0300

    Update segunda-aula.php

--- P1 2022-2/Web Dev/php/segunda-aula.php
+++ P1 2022-2/Web Dev/php/segunda-aula.php
@@ -1,8 +1,8 @@
 <?php
 
-    $nome = $_POST["nome"];
-    $nota1 = $_POST["nota1"];
-    $nota2 = $_POST["nota2"];
+    $nome = (string)$_POST["nome"];
+    $nota1 = (float) $_POST["nota1"];
+    $nota2 = (float) $_POST["nota2"];
     $media = ($nota1+$nota2)/2;
     echo "Nome: $nome <br> Notas: $nota1 | $nota2 <br> Média: $media"
 
