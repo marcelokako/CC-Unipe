bfa32b03f40f0a44bd4cdcee9dc8fe42c13ffc9f
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-09-21 19:43:30 -0300

    Add files via upload
    exercicio para enviar no dia 26/09/22

---
+++ P1 2022-2/TDA/acumulador.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(){
+    
+    int numero, contador = -1, acumulador = 0;
+    // contador eh -1 pois nao quero que o '0' usado para encerrar nao seja incluso no contador
+    do {
+        printf("Digite um numero para a soma acumulada\n(digite '0' para encerrar)\n");
+        scanf("%d", &numero);
+        acumulador += numero;
+        contador++;
+    } while (numero != 0);
+    printf("A soma acumulada dos %d numeros que voce digitou foi %d", contador, acumulador); 
+return 0;
+}
