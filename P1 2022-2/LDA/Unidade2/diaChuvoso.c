a2d0d8d0e20e8fa4c7a43914e25cee34db04c584
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-10-24 18:52:31 -0300

    Update diaChuvoso.c

--- P1 2022-2/LDA/Unidade2/diaChuvoso.c
+++ P1 2022-2/LDA/Unidade2/diaChuvoso.c
@@ -9,7 +9,7 @@ int main(){
 	int semanaChuva[7]; // em mm
 	char SEMANA[7][10] = {"Domingo", "Segunda", "Terca", "Quarta", "Quinta", "Sexta", "Sabado"};
 	int maisChuva, menosChuva;
-	int maisChuvaMM = -1;
+	int maisChuvaMM = 0;
 	int menosChuvaMM = 12;
 	int maisChuvaDia;
 	int menosChuvaDia;
@@ -33,7 +33,7 @@ int main(){
 	
 	printf("O dia que choveu menos foi: %s com %dmm", SEMANA[menosChuvaDia], semanaChuva[menosChuvaDia]);
 	printf("\nO dia que choveu mais foi: %s com %dmm", SEMANA[maisChuvaDia], semanaChuva[maisChuvaDia]);
-	printf("\nNo total choveu %.0fmm na semana e %f", acumulador, acumulador/7);
+	printf("\nNo total choveu %.0fmm na semana e media %fmm", acumulador, acumulador/7);
 	
 	return 0;
 }
