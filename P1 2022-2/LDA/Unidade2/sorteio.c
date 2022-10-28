752e6658117ad57416c7620cfd167541a5df3d42
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-10-28 15:27:58 -0300

    att sorteio.c
    novo exercicio para salvar multiplas posicoes corretas

--- P1 2022-2/LDA/Unidade2/sorteio.c
+++ P1 2022-2/LDA/Unidade2/sorteio.c
@@ -7,28 +7,39 @@
 int main(){
 	
 	srand(time(0));
-	int numeros[10], i, posicao = -1, palpite
+	int numeros[10], posicao[10];
+	int i, palpite;
+	int acerto = 0;
 	
 	for (i = 0; i < 10; i++){
-    	numeros[i] = rand() % 31;
+    	numeros[i] = rand() % 11;
 	}
 	printf("Digite um numero de 0 a 30: ");
 	scanf("%d", &palpite);
 	
 	for (i=0; i<10; i++){
 		if (palpite == numeros[i]){
-			posicao = i;
-		} 
+			posicao[i] = 1;
+			acerto = 1;
+		} else {
+			posicao[i] = 0;
+		}
 	}
-	
-	if (posicao > 0){
-		printf("\nVoce acertou!, o numero correto estava na posicao %d", posicao);
-	} else {
-		printf("\ntente novamente...");
-	}
+		if (acerto == 0){
+			printf("\nVoce nao acertou, tente novamente.");
+		} else {
+			printf("\nO(s) numero(s) correto(s) estava(m) na(s) posicao(oes): ");			
+			for (i = 0; i < 10; i++){
+				if (posicao[i] == 0){
+					continue;
+				} else {
+					printf("%d ", i);
+				}				
+			}
+		}
 	
 	for (i = 0; i < 10; i++){
-    	printf("\n%d ", numeros[i]);
+    	printf("\nPosicao [%d] | numero: %2.1d ", i, numeros[i]);
 	}
 	
 	return 0;
