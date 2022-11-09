2ac088f56e8100e5dd5a34f4643847e77fbd4f1d
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-11-09 19:54:50 -0300

    Add files via upload

---
+++ P1 2022-2/Web Dev/Unidade2/lista2-ex4-operacoes.php
@@ -0,0 +1,28 @@
+<?php
+
+include "lista2-imports.php";
+
+$numero1 = (float) $_POST["num1"];
+$numero2 = (float) $_POST["num2"];
+$operacao = $_POST["calculo"];
+
+switch ($_POST["calculo"]){
+    case "soma":
+        $resultado = soma([$numero1,$numero2]);
+        break;
+    case "subtracao":
+        $resultado = subtracao([$numero1,$numero2]);
+        break;
+    case "multiplicacao":
+        $resultado = multiplicacao([$numero1,$numero2]);
+        break;
+    case "divisao":
+        $resultado = divisao($numero1, $numero2);
+        break;
+    default:
+        $resultado = "ERRO";
+}
+
+echo "Operação: " . $_POST["calculo"] . "<br>Resultado: $resultado";
+
+?>
