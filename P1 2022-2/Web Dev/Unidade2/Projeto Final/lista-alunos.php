b7bad6d4021ea72fdc5cc5416f538bc5b8c70d06
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2022-11-14 19:20:58 -0300

    Add files via upload

---
+++ P1 2022-2/Web Dev/Unidade2/Projeto Final/lista-alunos.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Lista de Alunos</title>
+</head>
+<body>
+    <fieldset>
+        <legend>Lista de Aluno</legend>
+            <br>
+            Nome do aluno: <input type="text" name="nome"><br><br>
+            Matrícula: <input type="text" name="matricula"><br><br>
+            Nota 1: <input type="text" name="nota1"><br><br>
+            Nota 2: <input type="text" name="nota2"><br><br>
+            
+            <br>
+            <br>
+            <button>Cadastrar</button>
+    </fieldset>
+
+</body>
+</html>
