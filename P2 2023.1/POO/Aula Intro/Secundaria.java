88de0560addfe63cff732ef9ee5aa600f1561be0
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-03-01 10:23:15 -0300

    Create Secundaria.java

---
+++ P2 2023.1/POO/Aula Intro/Secundaria.java
@@ -0,0 +1,13 @@
+package aula01;
+
+public class Secundaria {
+
+	public void escreva() {
+		System.out.println("Entrei no escreva não estatico.");
+	}
+
+	public static void escrevaEstatico() {
+		System.out.println("Entrei no escreva estatico.");
+	}
+
+}
