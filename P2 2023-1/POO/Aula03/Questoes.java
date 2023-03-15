e03bec302510f4d9cea367f4442abca8aab44dd2
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-03-15 09:12:16 -0300

    Create Questoes.java

---
+++ P2 2023-1/POO/Aula03/Questoes.java
@@ -0,0 +1,23 @@
+package AulaPOO;
+
+import java.util.Scanner;
+
+public class Questoes {
+
+	public static void ex1() {
+		System.out.println("Exercicio 1");
+		Scanner entrada = new Scanner(System.in);
+		System.out.println("Digite o primeiro numero: ");
+		int num1 = entrada.nextInt();
+		System.out.println("Digite o segundo numero: ");
+		int num2 = entrada.nextInt();
+		
+		System.out.println("Soma: " + (num1+num2) + "\nSubtracao: " + (num1-num2));
+	}
+	
+	public static void ex2(){
+		System.out.println("Exercicio 2");
+
+	}
+	
+}
