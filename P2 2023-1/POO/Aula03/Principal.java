afe9ca565ee2ca2bab72fceaf48c2e4ee3cf16f2
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-03-15 09:11:52 -0300

    Create Principal.java

---
+++ P2 2023-1/POO/Aula03/Principal.java
@@ -0,0 +1,14 @@
+package AulaPOO;
+
+import java.util.Locale;
+
+public class Principal {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+		// TODO Auto-generated method stub
+		Questoes.ex1();
+
+	}
+
+}
