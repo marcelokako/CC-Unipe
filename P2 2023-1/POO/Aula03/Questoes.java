2767c67c748480d272599ef5298e0e9e4ed5d70d
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-03-15 10:30:48 -0300

    Update Questoes.java

--- P2 2023-1/POO/Aula03/Questoes.java
+++ P2 2023-1/POO/Aula03/Questoes.java
@@ -18,6 +18,45 @@ public class Questoes {
 	public static void ex2(){
 		System.out.println("Exercicio 2");
 
-	}
+		double n1 = Math.round(Math.random()*100);
+		double n2 = Math.round(Math.random()*100);
+		double n3 = Math.round(Math.random()*100);
 	
-}
+		System.out.println("Primeiro numero: " + n1 + 
+							"\nSegundo numero: " + n2 +
+							"\nTerceiro numero: " + n3);
+	}
+
+	public static void ex3(){
+	// AINDA INCOMPLETO	
+		System.out.println("Exercicio 3");
+		
+		double numeros[] = {0,0,0,0,0,0};
+		
+		int j = 0;
+		double x;
+		boolean repetido;
+		
+		while (j < 6) {
+			
+			repetido = true;
+			while (repetido == true) {
+			
+			x = Math.round(Math.random()*60);
+
+			for (int i=0;i<6;i++) {
+				if (x == numeros[i]) {
+					break;
+				}
+			}
+			
+			}
+			numeros[j] = x;
+			j++;
+		}
+		
+		for (int i=0;i<6;i++) {
+				System.out.println("Numero " + (i+1) + ": " + numeros[i]);
+			}
+		}
+	}
