2e91e92e9c93b87017cd82f43fed9a594b425355
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-03-22 10:26:31 -0300

    Create Principal.java

---
+++ P2 2023-1/POO/Aula04/Principal.java
@@ -0,0 +1,11 @@
+
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Secundaria.ex1(); // Calculadora
+		// Secundaria.ex2(); // media 3 notas string
+		 Secundaria.ex8(); // Operador Ternário
+	}
+
+}
