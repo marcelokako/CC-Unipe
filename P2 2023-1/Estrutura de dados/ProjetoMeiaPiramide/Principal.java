4b2c11850dfdb5cc0d883a85b3d01914bd8d8f44
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-03-30 08:20:12 -0300

    Create Principal.java

---
+++ P2 2023-1/Estrutura de dados/ProjetoMeiaPiramide/Principal.java
@@ -0,0 +1,13 @@
+public class Principal {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/* Quebre a string em formato de meia piramide, sem seguida, faça a soma do menor valor de cada linha
+		 * ex: "51 05 95 48 21 14"
+		 * 51
+		 * 05 95
+		 * 48 21 14
+		 */
+	}
+
+}
