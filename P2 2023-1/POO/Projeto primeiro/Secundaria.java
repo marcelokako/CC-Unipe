2e38211f7f9286fdb6c2f8b1e9daa2ce714b1028
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-04-05 17:49:56 -0300

    Update Secundaria.java

--- P2 2023-1/POO/Projeto primeiro/Secundaria.java
+++ P2 2023-1/POO/Projeto primeiro/Secundaria.java
@@ -8,10 +8,29 @@ public class Secundaria {
 		int temp;
 		for (int i=0; i<num;i++) {
 			temp = rand.nextInt(90) + 10; // gera numero entre 10 e 99  
-			resp = resp.concat(Integer.toString(temp));
-			resp = resp.concat(" ");
+			resp = resp.concat(Integer.toString(temp)); // adiciona numero à string
+			resp = resp.concat(" ");// adiciona espaço depois do numero
 		}
 		
 		return resp;
-	}
+	}//------------------------------------------------------------------gerador
+
+	public static String separador(String lista) {
+		String resp = "oi";
+		int tamanho = (lista.length()/3); // cada numero adicionado na lista possui 3 char
+		int linhas = 0;
+		int sequencia = 1;
+		int incremento = 1;
+		while (tamanho>=sequencia) {
+			
+			sequencia = sequencia + ++incremento;
+			//System.out.println(sequencia);
+
+			linhas++;
+		}
+		System.out.println(linhas);
+		return resp;
+	}//------------------------------------------------------------------separador
+
+	
 }
