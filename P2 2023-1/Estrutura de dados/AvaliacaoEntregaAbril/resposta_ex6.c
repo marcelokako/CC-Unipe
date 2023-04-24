0aae5efa3eceaffbc29a382ff60a5fb6bc30ad4b
duartemarco <marcelocamilogomes@gmail.com>
2023-04-24 10:27:05 -0300

    update resp6

--- P2 2023-1/Estrutura de dados/AvaliacaoEntregaAbril/resposta_ex6.c
+++ P2 2023-1/Estrutura de dados/AvaliacaoEntregaAbril/resposta_ex6.c
@@ -3,12 +3,12 @@
 #include <string.h>
 
 /* a) listas, b) pilhas e
-c) filas, com implementação sequencial e encadeada (dependendo da necessidade da aplicação). 
-Considerando que estas estruturas irão armazenar, em cada nó, o nome e o telefone de uma 
-pessoa, escreva as definições em C (typedef struct) destas três estruturas nas duas 
-implementações”*/
+c) filas, com implementacao sequencial e encadeada (dependendo da necessidade da aplicacao). 
+Considerando que estas estruturas irao armazenar, em cada no, o nome e o telefone de uma 
+pessoa, escreva as definicoes em C (typedef struct) destas tres estruturas nas duas 
+implementacoes�*/
 
-// nó com dados de cada pessoa
+// Dados de cada pessoa
 
 typedef struct elemento {
 	char[50] nome;
@@ -25,18 +25,31 @@ typedef struct letraA_Seq_Lista {
 
 typedef struct letraA_Enc_No {
 	dado_pessoa dado;
-	letraA_Enc_No * prox;
+	struct letraA_Enc_No * prox;
 } LetraA_No_ListaEncadeada;
 
-// b) Pilhas -NÃO CONCLUIDO, CONFERIR TUDO
+// b) Pilhas: Sequencial e Encadeada
 
-typedef struct letraA_Seq_Lista {
+typedef struct letraB_Pilha_Seq {
 	dado_pessoa dado;
-	int tamanho;
-	int topo
-} letraA_ListaSequencial;
+	int topo;
+} letraB_PilhaSequencial;
 
-typedef struct letraA_Enc_No {
+typedef struct letraB_Pilha_Enc_No {
 	dado_pessoa dado;
-	letraA_Enc_No * prox;
-} LetraA_No_ListaEncadeada;
+	struct letraB_Pilha_Enc_No * prox;
+} LetraB_No_PilhaEncadeada;
+
+// c) Filas: Sequencial e Encadeada
+
+typedef struct letraC_Fila_Seq {
+	dado_pessoa dado;
+	int inicio;
+	int fim;
+} letraC_PilhaSequencial;
+
+typedef struct letraC_Fila_Enc_No {
+	dado_pessoa dado;
+	struct letraC_Fila_Enc_No * prox;
+} LetraC_No_PilhaEncadeada;
+
