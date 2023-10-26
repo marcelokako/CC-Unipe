d40ee1c590a7221a9508bb40932f39a7b99f5252
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-10-26 09:37:48 -0300

    Update exercicios_sp.sql

--- P3 2023-2/Banco de dados/aula06_sp/exercicios_sp.sql
+++ P3 2023-2/Banco de dados/aula06_sp/exercicios_sp.sql
@@ -2,7 +2,7 @@ DELIMITER $$
 CREATE PROCEDURE sp_exercicio1()
 BEGIN 
 UPDATE livro SET livro.valor = livro.valor * 1.1
-WHERE livro.valor > 10;
+-- WHERE livro.valor > 10;
 END $$
 -- DROP PROCEDURE sp_exercicio1;
 CALL sp_exercicio1();
