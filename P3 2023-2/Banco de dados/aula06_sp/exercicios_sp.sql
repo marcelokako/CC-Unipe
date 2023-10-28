dc12b9f1b00e542efd8a10a2c952a312c5f21152
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2023-10-28 11:46:42 -0300

    exercicios slide sobre stored procedures finalizado

--- P3 2023-2/Banco de dados/aula06_sp/exercicios_sp.sql
+++ P3 2023-2/Banco de dados/aula06_sp/exercicios_sp.sql
@@ -1,8 +1,31 @@
 DELIMITER $$
 CREATE PROCEDURE sp_exercicio1()
 BEGIN 
-UPDATE livro SET livro.valor = livro.valor * 1.1
--- WHERE livro.valor > 10;
+UPDATE livro SET livro.valor = livro.valor * 1.1;
 END $$
--- DROP PROCEDURE sp_exercicio1;
+
 CALL sp_exercicio1();
+-- ---------------------------------------
+DELIMITER $$
+CREATE PROCEDURE sp_exercicio2()
+BEGIN 
+UPDATE livro SET livro.valor = livro.valor * 1.05
+WHERE livro.editora = 4;
+END $$
+
+CALL sp_exercicio2();
+-- ---------------------------------------
+DELIMITER $$
+CREATE PROCEDURE sp_exercicio3()
+BEGIN 
+SELECT 
+	livro.titulo,
+    livro.ano,
+    editora.nome,
+    assunto.nome
+FROM livro
+	INNER JOIN editora ON editora.codigo = livro.editora
+    INNER JOIN assunto ON assunto.sigla = livro.assunto;
+END $$
+
+CALL sp_exercicio3();
