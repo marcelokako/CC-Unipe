a48506e88ff58d4539911845894c8105f1ff2d43
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2024-03-05 07:57:39 -0300

    Update aula1_pixel.java

--- P4 2024-1/Processamento de imagem/aula1_pixel.java
+++ P4 2024-1/Processamento de imagem/aula1_pixel.java
@@ -1,13 +1,14 @@
 import javax.imageio.ImageIO;
+import javax.swing.*;
+import java.awt.*;
 import java.awt.image.BufferedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        File image_file = new File("img_samples/imagem.jpg");
+        BufferedImage imagem = pegarImagem("src/imagem.jpg");
 
-        BufferedImage imagem = ImageIO.read(image_file);
         int altura = imagem.getHeight();
         int largura = imagem.getWidth();
         int qtdPixel = altura * largura;
@@ -15,11 +16,42 @@ public class Main {
         System.out.println("Largura: " + largura);
         System.out.println("Quantidade de pixels: " + qtdPixel);
 
-        boolean salvo = ImageIO.write(imagem,"jpg",new File("img_samples/imagem2.jpg"));
-        if (salvo){
+        salvarImagem(imagem, "jpg", "imagem2.jpg");
+
+        exibirImagem("Processamento de imagem", imagem);
+    }
+
+    private static void exibirImagem(String titulo, BufferedImage... imagens) {
+        JFrame frame = new JFrame();
+        frame.setTitle(titulo);
+        frame.getContentPane().setLayout(new FlowLayout());
+        for (BufferedImage imagem : imagens){
+            JLabel label = new JLabel(new ImageIcon(imagem));
+            frame.getContentPane().add(label);
+        }
+
+        frame.pack();
+        frame.setVisible(true);
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    }
+
+    private static BufferedImage pegarImagem(String path) throws IOException {
+        try{
+            return ImageIO.read(new File(path));
+        } catch (IOException e){
+            System.out.println("Erro ao pegar a imagem");
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void salvarImagem(BufferedImage imagem, String formato, String nomeImagem) throws IOException {
+        try {
+            ImageIO.write(imagem,formato,new File(nomeImagem));
             System.out.println("Imagem salva com sucesso");
-        } else {
+            return;
+        } catch (IOException e){
             System.out.println("Erro ao salvar a imagem");
+            throw new RuntimeException(e);
         }
 
     }
