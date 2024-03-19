c6075162bde48e8ca7aef9457eb767f04977d116
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2024-03-19 08:34:34 -0300

    Update Main.java

--- P4 2024-1/Processamento de imagem/Main.java
+++ P4 2024-1/Processamento de imagem/Main.java
@@ -4,13 +4,13 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url_imagem = "exemplo2.png";
-        String url_imagem2= "exemplo2_VAR.png";
-        // String url_imagem = "src/exemplo.png";
+        //String url_imagem = "exemplo.png";
+        //String url_imagem2= "exemplo2_VAR.png";
+        String url_imagem = "src/exemplo.png";
 
         BufferedImage imagemAntes = ManipulaImg.pegarImagem(url_imagem);
         BufferedImage imagem = ManipulaImg.pegarImagem(url_imagem);
-        BufferedImage imagem2 = ManipulaImg.pegarImagem(url_imagem2);
+        //BufferedImage imagem2 = ManipulaImg.pegarImagem(url_imagem2);
 
         int altura = imagem.getHeight();
         int largura = imagem.getWidth();
@@ -21,31 +21,42 @@ public class Main {
 
         //exibeRGBPixel(imagem);
 
-        BufferedImage bandaRed = OperacoesManuais.bandasRGB(imagem, true, false, false);
-        BufferedImage bandaGreen = OperacoesManuais.bandasRGB(imagem, false, true, false);
-        BufferedImage bandaBlue = OperacoesManuais.bandasRGB(imagem, false, false, true);
+//        BufferedImage bandaRed = OperacoesManuais.bandasRGB(imagem, true, false, false);
+//        BufferedImage bandaGreen = OperacoesManuais.bandasRGB(imagem, false, true, false);
+//        BufferedImage bandaBlue = OperacoesManuais.bandasRGB(imagem, false, false, true);
 
-        BufferedImage negativo = OperacoesManuais.negativo(imagem);
-        BufferedImage bandaCinza = OperacoesManuais.bandaCinza(imagem);
-        BufferedImage bandaCinzaR = OperacoesManuais.bandaCinzaR(imagem);
-        BufferedImage bandaCinzaG = OperacoesManuais.bandaCinzaG(imagem);
-        BufferedImage bandaCinzaB = OperacoesManuais.bandaCinzaB(imagem);
-        
-        BufferedImage diferenca = OperacoesManuais.MostraDiferenca(imagem, imagem2);
+        // BufferedImage negativo = OperacoesManuais.negativo(imagem);
+//        BufferedImage bandaCinza = OperacoesManuais.bandaCinza(imagem);
+//        BufferedImage bandaCinzaR = OperacoesManuais.bandaCinzaR(imagem);
+//        BufferedImage bandaCinzaG = OperacoesManuais.bandaCinzaG(imagem);
+//        BufferedImage bandaCinzaB = OperacoesManuais.bandaCinzaB(imagem);
+
+        // BufferedImage diferenca = OperacoesManuais.MostraDiferenca(imagem, imagem2);
+        BufferedImage imagemBinarizada = OperacoesManuais.BinarizarImg(imagem, 100);
+        BufferedImage imagemTomRed = OperacoesManuais.aumentoTonalidade(imagem, "red", 150);
+        BufferedImage imagemTomGreen = OperacoesManuais.aumentoTonalidade(imagem, "Green", 150);
+        BufferedImage imagemTomBlue = OperacoesManuais.aumentoTonalidade(imagem, "Blue", 150);
+        BufferedImage imagemBrilhoAdd = OperacoesManuais.aumentoBrilhoAdd(imagem, -200);
+        BufferedImage imagemBrilhoMult = OperacoesManuais.aumentoBrilhoMult(imagem, 10);
 
         ManipulaImg.exibirImagem(
-                "Processamento de imagem", 
-                imagemAntes,
-                imagem2,
+                "Processamento de imagem",
+//                imagemAntes,
                 // bandaRed,
                 // bandaGreen,
                 // bandaBlue,
-                // bandaCinzaR,
-                // bandaCinzaG,
-                // bandaCinzaB,
-                diferenca,
-                negativo, 
-                bandaCinza);
+//                bandaCinzaR,
+//                bandaCinzaG,
+//                bandaCinzaB,
+                // negativo,
+                //bandaCinza
+                imagemBinarizada,
+                imagemTomRed,
+                imagemTomGreen,
+                imagemTomBlue,
+                imagemBrilhoAdd,
+                imagemBrilhoMult
+                );
         //ManipulaImg.salvarImagem(imagem, "png", "/exemplo_edit.png");
     }
 
