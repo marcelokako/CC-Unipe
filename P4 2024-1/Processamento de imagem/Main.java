014ac913c67749852f1daf4e11627b31b55f0966
Marcelo Camilo Gomes <marcelocamilogomes@gmail.com>
2024-04-17 20:56:08 -0300

    ADD Implementando Suavização por mediana e por média

--- P4 2024-1/Processamento de imagem/Main.java
+++ P4 2024-1/Processamento de imagem/Main.java
@@ -4,7 +4,7 @@ import java.io.IOException;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String url_imagem = "exemplo.png";
+        String url_imagem = "menino.png";
         //String url_imagem2= "exemplo2_VAR.png";
         // String url_imagem = "src/exemplo.png";
 
@@ -12,58 +12,14 @@ public class Main {
         BufferedImage imagem = ManipulaImg.pegarImagem(url_imagem);
         //BufferedImage imagem2 = ManipulaImg.pegarImagem(url_imagem2);
 
-        int altura = imagem.getHeight();
-        int largura = imagem.getWidth();
-        int qtdPixel = altura * largura;
-        System.out.println("Altura: " + altura);
-        System.out.println("Largura: " + largura);
-        System.out.println("Quantidade de pixels: " + qtdPixel);
-
-        //exibeRGBPixel(imagem);
-
-//        BufferedImage bandaRed = OperacoesManuais.bandasRGB(imagem, true, false, false);
-//        BufferedImage bandaGreen = OperacoesManuais.bandasRGB(imagem, false, true, false);
-//        BufferedImage bandaBlue = OperacoesManuais.bandasRGB(imagem, false, false, true);
-
-        BufferedImage negativo = OperacoesManuais.negativo(imagem);
-//        BufferedImage bandaCinza = OperacoesManuais.bandaCinza(imagem);
-//        BufferedImage bandaCinzaR = OperacoesManuais.bandaCinzaR(imagem);
-//        BufferedImage bandaCinzaG = OperacoesManuais.bandaCinzaG(imagem);
-//        BufferedImage bandaCinzaB = OperacoesManuais.bandaCinzaB(imagem);
-
-        // BufferedImage diferenca = OperacoesManuais.MostraDiferenca(imagem, imagem2);
-        // BufferedImage imagemBinarizada = OperacoesManuais.BinarizarImg(imagem, 100);
-        // BufferedImage imagemTomRed = OperacoesManuais.aumentoTonalidade(imagem, "red", 150);
-        // BufferedImage imagemTomGreen = OperacoesManuais.aumentoTonalidade(imagem, "Green", 150);
-        // BufferedImage imagemTomBlue = OperacoesManuais.aumentoTonalidade(imagem, "Blue", 150);
-        // BufferedImage imagemBrilhoAdd = OperacoesManuais.aumentoBrilhoAdd(imagem, -200);
-        // BufferedImage imagemBrilhoMult = OperacoesManuais.aumentoBrilhoMult(imagem, 10);
-        BufferedImage teste_conversao = OperacoesManuais.yiq2rgb(OperacoesManuais.rgb2yiq(imagem));
-        BufferedImage yiqBrilhoAdd = OperacoesManuais.aumentoBrilhoAdd(OperacoesManuais.rgb2yiq(imagem), 100);
-        BufferedImage yiqBrilhoMult = OperacoesManuais.aumentoBrilhoMult(OperacoesManuais.rgb2yiq(imagem), 2);
-        BufferedImage yiqNegativo = OperacoesManuais.negativo(OperacoesManuais.rgb2yiq(imagem));
+        BufferedImage suavizaMediana = OperacoesLocais.SuavizaImgMediana(imagem, 3);
+        BufferedImage suavizaMedia = OperacoesLocais.SuavizaImgMedia(imagem, 5);
 
         ManipulaImg.exibirImagem(
                 "Processamento de imagem",
                imagemAntes
-                // ,bandaRed
-                // ,bandaGreen
-                // ,bandaBlue
-                // ,bandaCinzaR
-                // ,bandaCinzaG
-                // ,bandaCinzaB
-                // ,negativo
-                // ,bandaCinza
-                // ,imagemBinarizada
-                // ,imagemTomRed
-                // ,imagemTomGreen
-                // ,imagemTomBlue
-                // ,imagemBrilhoAdd
-                // ,imagemBrilhoMult
-                ,teste_conversao
-                ,yiqBrilhoAdd
-                ,yiqBrilhoMult
-                ,yiqNegativo
+                ,suavizaMediana
+                ,suavizaMedia
                 );
 
         //ManipulaImg.salvarImagem(imagem, "png", "/exemplo_edit.png");
--- P4 2024-1/Processamento de imagem/OperacoesLocais.java
+++ P4 2024-1/Processamento de imagem/OperacoesLocais.java
@@ -1,21 +1,71 @@
 import java.awt.*;
 import java.awt.image.BufferedImage;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
 public class OperacoesLocais {
-    public static BufferedImage SuavizaImgMediana(BufferedImage imagemEntrada) {
+    public static BufferedImage SuavizaImgMediana(BufferedImage imagemEntrada, int dimensao) {
         int altura = imagemEntrada.getHeight();
         int largura = imagemEntrada.getWidth();
         
+        int parteInteira = dimensao / 2;
+
         BufferedImage imagemSaida = new BufferedImage(largura, altura, imagemEntrada.getType());
-        for (int x = 1; x < largura-1; x++) {
-            for (int y = 1; y < altura-1; y++) {
-                int[] arrayMediana = new int[9];
-                for (int i = 0; i < arrayMediana.length; i++) {
-                    
-                }
+        for (int l = parteInteira; l < largura - parteInteira; l++) {
+            for (int a = parteInteira; a < altura - parteInteira; a++) {
+
+                List<Integer> listaR = new ArrayList<>();
+                List<Integer> listaG = new ArrayList<>();
+                List<Integer> listaB = new ArrayList<>();
+                for (int i = 0; i < dimensao; i++) {
+                    for (int j = 0; j < dimensao; j++) {
+                        Color corPixel = new Color(imagemEntrada.getRGB(l-parteInteira+j, a-parteInteira+i));
+                        listaR.add(corPixel.getRed());
+                        listaG.add(corPixel.getGreen());
+                        listaB.add(corPixel.getBlue());
+                    }
+                }          
                 
-//                pintaPixelCor(imagemSaida, corNegativo, x, y); PINTA VALOR CENTRAL
-                
+                Collections.sort(listaR);
+                int corMedianaR = listaR.get(listaR.size() / 2);
+                Collections.sort(listaG);
+                int corMedianaG = listaG.get(listaG.size() / 2);
+                Collections.sort(listaB);
+                int corMedianaB = listaB.get(listaB.size() / 2);
+                OperacoesManuais.pintaPixelCor(imagemSaida, new Color(corMedianaR,corMedianaG,corMedianaB), l, a);   
+
+            }
+        }
+        return imagemSaida;
+    }
+
+    public static BufferedImage SuavizaImgMedia(BufferedImage imagemEntrada, int dimensao) {
+        int altura = imagemEntrada.getHeight();
+        int largura = imagemEntrada.getWidth();
+        
+        int parteInteira = dimensao / 2;
+        BufferedImage imagemSaida = new BufferedImage(largura, altura, imagemEntrada.getType());
+        for (int l = parteInteira; l < largura - parteInteira; l++) {
+            for (int a = parteInteira; a < altura - parteInteira; a++) {
+
+                double somaR = 0;
+                double somaG = 0;
+                double somaB = 0;
+                for (int i = 0; i < dimensao; i++) {
+                    for (int j = 0; j < dimensao; j++) {
+                        Color corPixel = new Color(imagemEntrada.getRGB(l-parteInteira+j, a-parteInteira+i));
+
+                        somaR += corPixel.getRed();
+                        somaG += corPixel.getGreen();
+                        somaB += corPixel.getBlue();
+                    }
+                }          
+                int corMediaR = (int) (somaR/(dimensao * dimensao));
+                int corMediaG = (int) (somaG/(dimensao * dimensao));
+                int corMediaB = (int) (somaB/(dimensao * dimensao));
+                OperacoesManuais.pintaPixelCor(imagemSaida, new Color(corMediaR,corMediaG,corMediaB), l, a);   
+
             }
         }
         return imagemSaida;
